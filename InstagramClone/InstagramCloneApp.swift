18943ead52594c6ae12154738e35d76d1c158946
Aryan Jagarwal <ajbaggar@gmail.com>
2023-06-05 11:24:30 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Aryan Jagarwal on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
