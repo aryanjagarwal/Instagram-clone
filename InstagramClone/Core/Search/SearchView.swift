05733356402aca16edf647bd0474ffd4c6793969
Aryan Jagarwal <ajbaggar@gmail.com>
2023-06-22 18:03:39 +0530

    added explore UI

---
+++ InstagramClone/Core/Search/SearchView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchView.swift
+//  InstagramClone
+//
+//  Created by Aryan Jagarwal on 22/06/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText = ""
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0 ... 15, id: \.self) { user in
+                        HStack {
+                            Image("IM4")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("Ironman")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Tony Stark")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
